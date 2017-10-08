de0ec6f57ecfe751c1c5f3541a88159000a07fb1
nnchen <32369145+nnchen@users.noreply.github.com>
2017-10-07 21:30:36 -0700

    Add files via upload

---
+++ AboutMe.java
@@ -0,0 +1,25 @@
+
+public class AboutMe 
+{
+	public static void main (String[] args) 
+	{
+		System.out.println("My favorite color is purple.");
+		System.out.println("I love eating at Piroshky.");
+		System.out.println("I've been working in the software industry for about three to four years.");
+		System.out.println("I love watching scary movies!");
+		System.out.println("Most recent scary movie I've seen was the movie IT, it was really good.");
+		System.out.println("I have a fiance named Alex and a dog named Bain. I love them very much.");
+		System.out.println("My mom is my Vegas buddy, we go together every year.");
+		System.out.println("I love all types of animals, but my favorite animals are the Slow Loris and Sloth.");
+		System.out.println("One day I would like to go into a shark cage.");
+		System.out.println("I use to live in Hawaii and I miss it.");
+	}
+}
+
+
+/* The difference between the println and print command  
+ * is that the println command sends an output to the
+ * current line and then it moves to the beginning of 
+ * the new line. Where as the print command will generate
+ * output all on the same line.
+ *  */
