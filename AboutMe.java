2c196eec0c4a8042601f02d8b59bca9f928974a1
nnchen <32369145+nnchen@users.noreply.github.com>
2017-10-07 20:51:06 -0700

    Add files via upload

---
+++ AboutMe.java
@@ -0,0 +1,18 @@
+
+public class AboutMe 
+{
+	public static void main (String[] args) 
+	{
+		System.out.println("My favorite color is purple.");
+		System.out.println("I love eating at Piroshky.");
+		System.out.println("I've been working in the software industry for about three to four years.");
+		System.out.println("I love watching scary movies!");
+		System.out.println("Most recent scary movie I've seen was the movie IT, it was really good.");
+		System.out.println("I have a fiance named Alex and a dog named Bain. I love them very much.");
+		System.out.println("My mom is my Vegas buddy, we go together every year.");
+		System.out.println("I love all types of animals, but my favorite animals are the Slow Loris and Sloth.");
+		System.out.println("One day I would like to go into a shark cage.");
+		System.out.println("I use to live in Hawaii and I miss it.");
+		
+	}
+}
